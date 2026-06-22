ecdea1bd2220458365e5f463b7b1c57f27243b30
Azka Azhar <azkazhar2006@gmail.com>
2026-06-23 00:32:54 +0500

    added new comment

--- Game/Form1.cs
+++ Game/Form1.cs
@@ -13,8 +13,16 @@ namespace Game
     public partial class Form1 : Form
     {
         public Form1()
+
         {
+            //starting
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
         }
     }
 }
