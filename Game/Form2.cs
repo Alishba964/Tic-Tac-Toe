debeca6858c4aa1bbef52164493b12ccb44c3768
Alishba964 <alishba.khan191206@gmail.com>
2026-06-25 01:02:40 +0500

    form2 connected

---
+++ Game/Form2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Game
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 gameform = new Form1();
+            gameform.Show();
+            this.Hide();
+        }
+    }
+}
